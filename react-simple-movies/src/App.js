0ae06f0b24211056f06b4c88d13b480ca4f53930
evondev <trananhtuan400@gmail.com>
2022-03-08 15:06:13 +0700

    Optimize

--- react-simple-movies/src/App.js
+++ react-simple-movies/src/App.js
@@ -1,33 +1,40 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
 import Banner from "./components/banner/Banner";
 import Main from "./components/layout/Main";
-import HomePage from "./pages/HomePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
-import MoviePage from "./pages/MoviePage";
+// import HomePage from "./pages/HomePage";
+// import MovieDetailsPage from "./pages/MovieDetailsPage";
+// import MoviePage from "./pages/MoviePage";
+// dynamic import
+// Advanced react pattern
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner></Banner>
-                <HomePage></HomePage>
-              </>
-            }
-          ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movie/:movieId"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
