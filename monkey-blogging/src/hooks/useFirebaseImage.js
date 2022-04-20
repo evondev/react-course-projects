4fbdf104906a4cdbbdaf0c44b2d645e5f0e8645b
evondev <trananhtuan400@gmail.com>
2022-04-21 00:27:55 +0700

    Convert firebase image upload to custom hook

---
+++ monkey-blogging/src/hooks/useFirebaseImage.js
@@ -0,0 +1,73 @@
+import {
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useState } from "react";
+
+export default function useFirebaseImage(setValue, getValues) {
+  const [progress, setProgress] = useState(0);
+  const [image, setImage] = useState("");
+  if (!setValue || !getValues) return;
+  const handleUploadImage = (file) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progressPercent =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progressPercent);
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing at all");
+        }
+      },
+      (error) => {
+        console.log("Error");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          setImage(downloadURL);
+        });
+      }
+    );
+  };
+  const handleSelectImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image_name", file.name);
+    handleUploadImage(file);
+  };
+
+  const handleDeleteImage = () => {
+    const storage = getStorage();
+    const imageRef = ref(storage, "images/" + getValues("image_name"));
+    deleteObject(imageRef)
+      .then(() => {
+        console.log("Remove image successfully");
+        setImage("");
+        setProgress(0);
+      })
+      .catch((error) => {
+        console.log("handleDeleteImage ~ error", error);
+        console.log("Can not delete image");
+      });
+  };
+  return {
+    image,
+    progress,
+    handleSelectImage,
+    handleDeleteImage,
+  };
+}
--- monkey-blogging/src/module/post/PostAddNew.js
+++ monkey-blogging/src/module/post/PostAddNew.js
@@ -1,25 +1,15 @@
 import { Button } from "components/button";
-import { Checkbox, Radio } from "components/checkbox";
-import { Dropdown } from "components/dropdown";
+import { Radio } from "components/checkbox";
 import { Field } from "components/field";
 import { Input } from "components/input";
 import { Label } from "components/label";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import slugify from "slugify";
 import styled from "styled-components";
 import { postStatus } from "utils/constants";
-// import ImageUpload from "components/image/ImageUpload";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  deleteObject,
-} from "firebase/storage";
 import ImageUpload from "components/image/ImageUpload";
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "firebase-app/firebase-config";
+import useFirebaseImage from "hooks/useFirebaseImage";
 
 const PostAddNewStyles = styled.div``;
 const PostAddNew = () => {
@@ -33,68 +23,15 @@ const PostAddNew = () => {
     },
   });
   const watchStatus = watch("status");
-  const watchCategory = watch("category");
+  // const watchCategory = watch("category");
   const addPostHandler = async (values) => {
     const cloneValues = { ...values };
     cloneValues.slug = slugify(values.slug || values.title);
     cloneValues.status = Number(values.status);
   };
-  const [progress, setProgress] = useState(0);
-  const [image, setImage] = useState("");
-  const handleUploadImage = (file) => {
-    const storage = getStorage();
-    const storageRef = ref(storage, "images/" + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progressPercent =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progressPercent);
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-          default:
-            console.log("Nothing at all");
-        }
-      },
-      (error) => {
-        console.log("Error");
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          setImage(downloadURL);
-        });
-      }
-    );
-  };
-  const onSelectImage = (e) => {
-    const file = e.target.files[0];
-    console.log("onSelectImage ~ file", file);
-    if (!file) return;
-    setValue("image_name", file.name);
-    handleUploadImage(file);
-  };
 
-  const handleDeleteImage = () => {
-    const storage = getStorage();
-    const imageRef = ref(storage, "images/" + getValues("image_name"));
-    deleteObject(imageRef)
-      .then(() => {
-        console.log("Remove image successfully");
-        setImage("");
-        setProgress(0);
-      })
-      .catch((error) => {
-        console.log("handleDeleteImage ~ error", error);
-        console.log("Can not delete image");
-      });
-  };
+  const { image, progress, handleSelectImage, handleDeleteImage } =
+    useFirebaseImage(setValue, getValues);
 
   return (
     <PostAddNewStyles>
@@ -123,7 +60,7 @@ const PostAddNew = () => {
           <Field>
             <Label>Image</Label>
             <ImageUpload
-              onChange={onSelectImage}
+              onChange={handleSelectImage}
               handleDeleteImage={handleDeleteImage}
               className="h-[250px]"
               progress={progress}
