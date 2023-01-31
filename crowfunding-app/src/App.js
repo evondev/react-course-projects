d86610551e34c82c9e97f50fb95d5d46796c102a
evondev <trananhtuan400@gmail.com>
2023-01-31 23:47:57 +0700

    upgrade react router dom

--- crowfunding-app/src/App.js
+++ crowfunding-app/src/App.js
@@ -1,44 +1,14 @@
-import LayoutDashboard from "layout/LayoutDashboard";
-import CampaignView from "modules/campaign/CampaignView";
-import React, { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
 import Modal from "react-modal";
-import LayoutPayment from "layout/LayoutPayment";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { authRefreshToken, authUpdateUser } from "./store/auth/auth-slice";
 import { getToken, logOut } from "utils/auth";
-import axios from "axios";
-import RequiredAuthPage from "pages/RequiredAuthPage";
-import { permissions } from "constants/permissions";
-const SignUpPage = lazy(() => import("./pages/SignUpPage"));
-const SignInPage = lazy(() => import("./pages/SignInPage"));
-const DashboardPage = lazy(() => import("./pages/DashboardPage"));
-const CampaignPage = lazy(() => import("./pages/CampaignPage"));
-const StartCampaignPage = lazy(() => import("./pages/StartCampaignPage"));
-const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
-const ShippingPage = lazy(() => import("./pages/ShippingPage"));
-const PaymentPage = lazy(() => import("./pages/PaymentPage"));
-const WithdrawPage = lazy(() => import("./pages/WithdrawPage"));
-const UnauthorizePage = lazy(() => import("./pages/UnauthorizePage"));
 
 Modal.setAppElement("#root");
 Modal.defaultStyles = {};
 
 function App() {
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await axios.post(
-  //       "/demo",
-  //       {},
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer Token`,
-  //         },
-  //       }
-  //     );
-  //   }
-  // }, []);
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -60,56 +30,7 @@ function App() {
       }
     }
   }, [dispatch, user]);
-  return (
-    <Suspense>
-      <Routes>
-        <Route element={<LayoutDashboard></LayoutDashboard>}>
-          <Route path="/" element={<DashboardPage></DashboardPage>}></Route>
-          <Route
-            path="/unauthorize"
-            element={<UnauthorizePage></UnauthorizePage>}
-          ></Route>
-          <Route
-            path="/withdraw"
-            element={<WithdrawPage></WithdrawPage>}
-          ></Route>
-          <Route path="/payment" element={<PaymentPage></PaymentPage>}></Route>
-          <Route
-            path="/campaign"
-            element={<CampaignPage></CampaignPage>}
-          ></Route>
-          <Route
-            element={
-              <RequiredAuthPage
-                allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}
-              ></RequiredAuthPage>
-            }
-          >
-            <Route
-              path="/start-campaign"
-              element={<StartCampaignPage></StartCampaignPage>}
-            ></Route>
-          </Route>
-          <Route
-            path="/campaign/:slug"
-            element={<CampaignView></CampaignView>}
-          ></Route>
-        </Route>
-        <Route element={<LayoutPayment></LayoutPayment>}>
-          <Route
-            path="/checkout"
-            element={<CheckoutPage></CheckoutPage>}
-          ></Route>
-          <Route
-            path="/shipping-address"
-            element={<ShippingPage></ShippingPage>}
-          ></Route>
-        </Route>
-        <Route path="/register" element={<SignUpPage></SignUpPage>}></Route>
-        <Route path="/login" element={<SignInPage></SignInPage>}></Route>
-      </Routes>
-    </Suspense>
-  );
+  return <></>;
 }
 
 export default App;
--- crowfunding-app/src/index.js
+++ crowfunding-app/src/index.js
@@ -1,25 +1,98 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/configureStore";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LayoutDashboard from "layout/LayoutDashboard";
 
+import { permissions } from "constants/permissions";
+import RequiredAuthPage from "pages/RequiredAuthPage";
+import CampaignView from "modules/campaign/CampaignView";
+const SignUpPage = lazy(() => import("./pages/SignUpPage"));
+const SignInPage = lazy(() => import("./pages/SignInPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const CampaignPage = lazy(() => import("./pages/CampaignPage"));
+const StartCampaignPage = lazy(() => import("./pages/StartCampaignPage"));
+const PaymentPage = lazy(() => import("./pages/PaymentPage"));
+const WithdrawPage = lazy(() => import("./pages/WithdrawPage"));
+const UnauthorizePage = lazy(() => import("./pages/UnauthorizePage"));
 const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+const router = createBrowserRouter([
+  {
+    element: <LayoutDashboard></LayoutDashboard>,
+    children: [
+      {
+        path: "/",
+        element: <DashboardPage></DashboardPage>,
+      },
+      {
+        path: "/campaign",
+        element: <CampaignPage></CampaignPage>,
+      },
+      {
+        path: "/payment",
+        element: <PaymentPage></PaymentPage>,
+      },
+      {
+        path: "/withdraw",
+        element: <WithdrawPage></WithdrawPage>,
+      },
+      {
+        path: "/unauthorize",
+        element: <UnauthorizePage></UnauthorizePage>,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <SignUpPage></SignUpPage>,
+  },
+  {
+    path: "/login",
+    element: <SignInPage></SignInPage>,
+  },
+  {
+    element: (
+      <RequiredAuthPage
+        allowPermissions={[permissions.campaign.CREATE_CAMPAIGN]}
+      ></RequiredAuthPage>
+    ),
+    children: [
+      {
+        path: "/start-campaign",
+        element: <StartCampaignPage></StartCampaignPage>,
+      },
+      {
+        path: "/campaign/:slug",
+        element: <CampaignView></CampaignView>,
+      },
+    ],
+  },
+]);
+createRoot(container).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <ToastContainer bodyClassName="font-primary text-sm"></ToastContainer>
-    </BrowserRouter>
+    <Suspense fallback={<p></p>}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Suspense>
+    <ToastContainer bodyClassName="font-primary text-sm"></ToastContainer>
   </Provider>
 );
+
+// root.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//       <ToastContainer bodyClassName="font-primary text-sm"></ToastContainer>
+//     </BrowserRouter>
+//   </Provider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
